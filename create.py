fad723244bb50bedd16994b12d31cd6fee6a2e78
Benji <97198967+BenPI88@users.noreply.github.com>
2023-02-26 11:21:07 -0600

    Update create.py

--- create.py
+++ create.py
@@ -4,10 +4,12 @@ import random
 i = 0
 rndstr = ""
 while not i == 10000:
+  i += 1
   os.system("clear")
   rndstr = rndstr + string.ascii_uppercase[random.randint(0, len(string.ascii_uppercase)) - 1]
   print("Generating Random String...")
   print(str(i) + "/10000")
+os.system("echo " + rndstr + " > rndstr.txt")
 os.system("clear")
 os.system('sudo tar -zcvf zbomb1.tar.gz rndstr.txt')
 while True:
